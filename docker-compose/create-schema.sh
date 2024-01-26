c7502d321dd035751efbbd664c444f1827bd7e67
Jaroslav Beran <jaroslav.beran@dhl.com>
2024-01-26 17:28:36 +0100

    Select query

--- docker-compose/create-schema.sh
+++ docker-compose/create-schema.sh
@@ -2,5 +2,5 @@ USER=sysdba
 PASSWORD=change-it
 DATABASE=MYPOCDB
 
-/usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/database.sql' "localhost:$DATABASE"
+/usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/database.sql' "poc-db:$DATABASE"
 rm /firebird/database.sql

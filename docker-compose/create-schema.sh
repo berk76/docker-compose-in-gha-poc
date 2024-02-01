bb5f732f170734e6ee8c670ee35bd5c941eda613
Jaroslav Beran <jaroslav.beran@dhl.com>
2024-02-01 17:53:47 +0100

    test1

--- docker-compose/create-schema.sh
+++ docker-compose/create-schema.sh
@@ -2,6 +2,7 @@ USER=sysdba
 PASSWORD=change-it
 DATABASE=MYPOCDB
 
+sleep 1s
 cat /etc/hosts
 /usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/database.sql' "host.docker.internal:$DATABASE"
 rm /firebird/database.sql

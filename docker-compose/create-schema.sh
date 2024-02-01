dfd3f4dda0e12121e58c968036f3d5d3c9d7607a
Jaroslav Beran <jaroslav.beran@dhl.com>
2024-02-01 17:25:23 +0100

    test1

--- docker-compose/create-schema.sh
+++ docker-compose/create-schema.sh
@@ -2,6 +2,5 @@ USER=sysdba
 PASSWORD=change-it
 DATABASE=MYPOCDB
 
-systemctl stop firewalld.service
-/usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/database.sql' "localhost:$DATABASE"
+/usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/database.sql' "poc-db:$DATABASE"
 rm /firebird/database.sql

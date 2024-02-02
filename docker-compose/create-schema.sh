c2d7652a99810ff4cb143bbacb1c68ff3bcc19b4
Jaroslav Beran <jaroslav.beran@dhl.com>
2024-02-02 09:11:14 +0100

    Selenium test

--- docker-compose/create-schema.sh
+++ docker-compose/create-schema.sh
@@ -3,6 +3,6 @@ PASSWORD=change-it
 DATABASE=MYPOCDB
 
 sleep 1s
-cat /etc/hosts
+# cat /etc/hosts
 /usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/database.sql' "host.docker.internal:$DATABASE"
 rm /firebird/database.sql
